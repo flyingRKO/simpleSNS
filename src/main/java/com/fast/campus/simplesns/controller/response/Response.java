ecd9ca19a437cca65919585cac1e92ae525f22ec
Lee Jae Hun <wogns970@naver.com>
2023-02-17 18:19:41 +0900

    프론트 코드를 위한 설정 코드 추가

--- src/main/java/com/fast/campus/simplesns/controller/response/Response.java
+++ src/main/java/com/fast/campus/simplesns/controller/response/Response.java
@@ -19,6 +19,14 @@ public class Response<T> {
     }
 
     public String toStream() {
+
+        if (result == null){
+            return "{" +
+                    "\"resultCode\":" + "\"" + resultCode + "\"," +
+                    "\"resultMessage\":" + "\"" + resultMessage + "\"," +
+                    "\"result\":" + result +
+                    "}";
+        }
         return "{" +
                 "\"resultCode\":" + "\"" + resultCode + "\"," +
                 "\"resultMessage\":" + "\"" + resultMessage + "\"," +
