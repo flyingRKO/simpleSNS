3486eab2c830f8198ac22cd4cdf21838990d0dc3
Lee Jae Hun <wogns970@naver.com>
2023-03-02 17:11:47 +0900

    테스트 코드 추가
    - 좋아요 테스트 코드 추가
    - 댓글 테스트 코드 추가
    - 알람 테스트 코드 추가

--- src/test/java/com/fast/campus/simplesns/controller/PostControllerTest.java
+++ src/test/java/com/fast/campus/simplesns/controller/PostControllerTest.java
@@ -1,5 +1,6 @@
 package com.fast.campus.simplesns.controller;
 
+import com.fast.campus.simplesns.controller.request.PostCommentRequest;
 import com.fast.campus.simplesns.controller.request.PostModifyRequest;
 import com.fast.campus.simplesns.controller.request.PostWriteRequest;
 import com.fast.campus.simplesns.exception.ErrorCode;
@@ -53,7 +54,7 @@ public class PostControllerTest {
     }
 
     @Test
-    @WithMockUser
+    @WithAnonymousUser
     void 포스트작성시_로그인하지않은경우_에러발생() throws Exception {
 
         mockMvc.perform(post("/api/v1/posts")
@@ -205,6 +206,68 @@ public class PostControllerTest {
                 ).andDo(print())
                 .andExpect(status().isUnauthorized());
 
+    }
+
+    // 좋아요 기능
+    @Test
+    @WithMockUser
+    void 좋아요기능() throws Exception {
+        mockMvc.perform(post("/api/v1/posts/1/likes")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithAnonymousUser
+    void 좋아요클릭시_로그인하지_않은경우() throws Exception {
+        mockMvc.perform(post("/api/v1/posts/1/likes")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
+    void 좋아요클릭시_게시물이_없는경우() throws Exception {
+        doThrow(new SimpleSnsApplicationException(ErrorCode.POST_NOT_FOUND)).when(postService).like(any(),any());
+        mockMvc.perform(post("/api/v1/posts/1/likes")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    // 댓글 기능
+
+    @Test
+    @WithMockUser
+    void 댓글기능() throws Exception {
+        mockMvc.perform(post("/api/v1/posts/1/comments")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new PostCommentRequest("comment")))
+                ).andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithAnonymousUser
+    void 댓글작성시_로그인하지_않은경우() throws Exception {
+        mockMvc.perform(post("/api/v1/posts/1/comments")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new PostCommentRequest("comment")))
+                ).andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
+    void 댓글작성시_게시물이_없는경우() throws Exception {
+        doThrow(new SimpleSnsApplicationException(ErrorCode.POST_NOT_FOUND)).when(postService).comment(any(), any(), any());
+        mockMvc.perform(post("/api/v1/posts/1/comments")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new PostCommentRequest("comment")))
+                ).andDo(print())
+                .andExpect(status().isNotFound());
     }
 
 
--- src/test/java/com/fast/campus/simplesns/service/UserServiceTest.java
+++ src/test/java/com/fast/campus/simplesns/service/UserServiceTest.java
@@ -9,18 +9,31 @@ import com.fast.campus.simplesns.repository.UserEntityRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class UserServiceTest {
 
+    @Autowired
+    private MockMvc mockMvc;
     @Autowired
     UserService userService;
 
@@ -88,6 +101,28 @@ public class UserServiceTest {
                 () -> userService.join(fixture.getUserName(), fixture.getPassword()));
 
         Assertions.assertEquals(ErrorCode.DUPLICATED_USER_NAME, exception.getErrorCode());
+    }
+
+    // 알람 기능
+
+    @Test
+    @WithMockUser
+    void 알람기능() throws Exception {
+        when(userService.alarmList(any(), any())).thenReturn(Page.empty());
+        mockMvc.perform(get("/api/v1/users/alarm")
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithAnonymousUser
+    void 알람기능시_로그인하지_않은경우() throws Exception {
+        when(userService.alarmList(any(), any())).thenReturn(Page.empty());
+        mockMvc.perform(get("/api/v1/users/alarm")
+                        .contentType(MediaType.APPLICATION_JSON)
+                ).andDo(print())
+                .andExpect(status().isUnauthorized());
     }
 
 }
